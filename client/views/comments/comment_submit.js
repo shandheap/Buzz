a5d29dfc0bc1b9c5eef95da2c92b17a25f12ebb1
Shandheap Shanmuganathan <shandheap@gmail.com>
2013-12-22 11:57:51 +0530

    Improve comment system.

--- client/views/comments/comment_submit.js
+++ client/views/comments/comment_submit.js
@@ -5,10 +5,13 @@ Template.commentSubmit.events({
 		var comment = {
 			body: $(event.target).find('[name=body]').val(),
 			postId: template.data._id
-		}
+		};
+
+		// Clean the form input
+		$(event.target).find('[name=body]').val("");
 
 		Meteor.call('comment', comment, function(error, commentId) {
-			error && throwError(error.reason);
+			error && Meteor.Errors.throw(error.reason);
 		});
 	}
 });
